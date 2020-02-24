d262a4409e1f5b1fce67c0a77a054187100640de
Anthony BIARD <39205962+kibonara@users.noreply.github.com>
2020-02-24 23:20:26 +0100

    Update spawnVeh.lua

--- spawnVeh.lua
+++ spawnVeh.lua
@@ -1,34 +1,27 @@
 
-RegisterCommand('car', function(source, args)
+function ShowNotification( text )
+    SetNotificationTextEntry("STRING")
+    AddTextComponentSubstringPlayerName(text)
+    DrawNotification(false, false)
+end
+
+RegisterCommand('car', function(source, args, rawCommand)
+    local x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 8.0, 0.5))
+    local veh = args[1]
+    if veh == nil then veh = "adder" end
+    vehiclehash = GetHashKey(veh)
+    RequestModel(vehiclehash)
     
-    local vehicleName = args[1] or 'adder'
-
-    if not IsModelInCdimage(vehicleName) or not IsModelAVehicle(vehicleName) then
-        TriggerEvent('chat:addMessage', {
-            args = { 'It might have been a good thing that you tried to spawn a ' .. vehicleName .. '. Who even wants their spawning to actually ^*succeed?' }
-        })
-
-        return
-    end
-
-    RequestModel(vehicleName)
-
-    while not HasModelLoaded(vehicleName) do
-        Wait(500)
-    end
-
-    local playerPed = PlayerPedId()
-    local pos = GetEntityCoords(playerPed)
-
-    local vehicle = CreateVehicle(vehicleName, pos.x, pos.y, pos.z, GetEntityHeading(playerPed), true, false)
-
-    SetPedIntoVehicle(playerPed, vehicle, -1)
-
-    SetEntityAsNoLongerNeeded(vehicle)
-
-    SetModelAsNoLongerNeeded(vehicleName)
-
-    TriggerEvent('chat:addMessage', {
-		args = { 'Woohoo! Enjoy your new ^*' .. vehicleName .. '!' }
-	})
-end, false)
+    Citizen.CreateThread(function() 
+        local waiting = 0
+        while not HasModelLoaded(vehiclehash) do
+            waiting = waiting + 100
+            Citizen.Wait(100)
+            if waiting > 5000 then
+                ShowNotification("~r~Could not load the vehicle model in time, a crash was prevented.")
+                break
+            end
+        end
+        CreateVehicle(vehiclehash, x, y, z, GetEntityHeading(PlayerPedId())+90, 1, 0)
+    end)
+end)
