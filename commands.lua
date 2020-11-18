78bc823008f228b4aae3b051339b0e9481437645
Marco Polo <39205962+kibonara@users.noreply.github.com>
2020-11-18 06:01:49 +0100

    Delete commands.lua

--- commands.lua
+++
@@ -1,8 +0,0 @@
-RegisterCommand("help", funtion()
-  msg("Server's Discord : discord.gg/RfN4UC5")
-  msg("Server's Website : website.com")
-end, false)
-
-function msg(text)
-  TriggerEvent("chatMessage", "[Server]", {255,0,0}, text)
- end
